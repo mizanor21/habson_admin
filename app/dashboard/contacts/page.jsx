ecf47638bda62c9bbc96ccda9c23bf58130b2a69
mizanor21 <mizanorrahman398@gmail.com>
2025-02-15 13:25:25 +0600

    contact image part polishing

--- app/dashboard/contacts/page.jsx
+++ app/dashboard/contacts/page.jsx
@@ -1,7 +1,7 @@
 export const revalidate = 10; // Revalidate every 10 second
-import { Contact } from "@/app/lib/Contact/model";
-import ContactImg from "@/app/ui/ContactImg/ContactImg";
+import Image from "next/image";
 import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Contacts = async () => {
   const res = await fetch("http://localhost:3000/api/contact-img", {
@@ -17,7 +17,23 @@ const Contacts = async () => {
   const contactImg = await res.json();
 
   return <div>
-    <ContactImg data={contactImg} />
+    <div>
+      <div className="mb-5">
+        <h1 className="text-3xl font-bold uppercase">Contact Images</h1>
+      </div>
+      <div className="grid grid-cols-3 gap-3">
+        {contactImg.map((item, index) => (
+          <div key={index} className="relative group">
+            <Image width={500} height={500} src={item.img} alt="contact" />
+            <div className="absolute hidden  right-5 top-5 group-hover:flex">
+              <button className="bg-white p-2 rounded-full hover:bg-red-500 hover:text-white transition-all duration-500">
+                <AiOutlineDelete size={20} />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   </div>;
 };
 
