cbc066be7484b5477fb92919f6191eeb4fa4de16
mizanor21 <mizanorrahman398@gmail.com>
2025-02-20 11:07:49 +0600

    some polishing

--- components/custom/DataFetch.jsx
+++ components/custom/DataFetch.jsx
@@ -4,7 +4,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export const useNewsItemsData = () => {
   const { data, error } = useSWR(
-    "https://living-brands-admin.vercel.app/api/news-center",
+    `${process.env.NEXT_PUBLIC_API_URL}/api/news-center`,
     fetcher
   );
   return { data, error, isLoading: !data && !error };
